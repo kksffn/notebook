d7a062c01be6d13ddbd5923cd9f2c5b21e8a4c22
kksffn <ivohradecky79@gmail.com>
2020-10-28 16:42:06 +0100

    little changes in config

--- src/main/java/cz/kksffn/logging/MyPatternLayout.java
+++
@@ -1,9 +0,0 @@
-package cz.kksffn.logging;
-
-import cz.kksffn.App;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class MyPatternLayout {
-    private static Logger logger = LoggerFactory.getLogger(App.class);
-}
